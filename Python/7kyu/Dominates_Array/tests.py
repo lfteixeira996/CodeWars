5792b1a8505c180c689cd85d93f3e0c6a7987956
Luis Teixeira <filipe.teixeira.996@gmail.com>
2021-02-09 19:00:25 +0000

    Update tests.py

--- Python/7kyu/Dominates_Array/tests.py
+++ Python/7kyu/Dominates_Array/tests.py
@@ -4,8 +4,11 @@
 #Date   : 19-05-2020  
 #E-Mail : filipe.teixeira.996@gmail.com  
 ##############################################  
-Test.assert_equals(dominator([3,4,3,2,3,1,3,3]),3)
-Test.assert_equals(dominator([1,2,3,4,5]),-1)
-Test.assert_equals(dominator([1,1,1,2,2,2]),-1)
-Test.assert_equals(dominator([1,1,1,2,2,2,2]),2)
-Test.assert_equals(dominator([]),-1)
+@test.describe('Example Tests')
+def example_tests():
+
+    test.assert_equals(dominator([3,4,3,2,3,1,3,3]),3)
+    test.assert_equals(dominator([1,2,3,4,5]),-1)
+    test.assert_equals(dominator([1,1,1,2,2,2]),-1)
+    test.assert_equals(dominator([1,1,1,2,2,2,2]),2)
+    test.assert_equals(dominator([]),-1)
